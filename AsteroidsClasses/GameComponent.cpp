91dfd644a82a10da46a12903210a239cbb5eac7c
unknown <skooter500>
2014-09-25 17:15:44 +0100

    Bla

--- AsteroidsClasses/GameComponent.cpp
+++ AsteroidsClasses/GameComponent.cpp
@@ -4,7 +4,6 @@ using namespace Game1;
 
 GameComponent::GameComponent(void)
 {
-	std::cout << "Base class constructor" << std::endl;
 	position = glm::vec2(0.0f);
 	look = glm::vec2(0.0f, 1.0f);
 	rotation = 0.0f;
